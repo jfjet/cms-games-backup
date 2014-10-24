b359b3b72f2fdd69e076a891cf289c141a301b24
Min Hur <min.hur@nytimes.com>
2014-10-24 17:41:30 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@
 
 # rsync -rv --exclude '.*' ../cms-game webl@cms-sandbox01.dev.ewr1.nytimes.com:/var/www
 
-rsync -rv --exclude '.*' ../cms-game webl@cms-frontend01.dev.ewr1.nytimes.com:/opt/nginx
-rsync -rv --exclude '.*' ../cms-game webl@cms-frontend02.dev.ewr1.nytimes.com:/opt/nginx
+rsync -rv --exclude '.*' ../cms-game webl@cms-frontend03.prd.ewr1.nytimes.com:/opt/nginx
+rsync -rv --exclude '.*' ../cms-game webl@cms-frontend04.prd.ewr1.nytimes.com:/opt/nginx
