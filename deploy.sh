a312185a02b3704c675113776d5ef77cb40f89b2
Min Hur <min.hur@nytimes.com>
2014-10-24 17:49:31 -0400

    chmod first before deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@
 
 # rsync -rv --exclude '.*' ../cms-game webl@cms-sandbox01.dev.ewr1.nytimes.com:/var/www
 
+chmod a+x roms/*
+chmod a+r roms/*
 rsync -rv --exclude '.*' ../cms-game webl@cms-frontend03.prd.ewr1.nytimes.com:/opt/nginx
 rsync -rv --exclude '.*' ../cms-game webl@cms-frontend04.prd.ewr1.nytimes.com:/opt/nginx
